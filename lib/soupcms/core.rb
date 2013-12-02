01c6bb559760977fa4b41e8a9540004722c3f359
Sunit Parekh <parekh.sunit@gmail.com>
2013-12-02 10:15:28 +0530

    initial commit

---
+++ lib/soupcms/core.rb
@@ -0,0 +1,7 @@
+require "soupcms/core/version"
+
+module Soupcms
+  module Core
+    # Your code goes here...
+  end
+end
---
+++ lib/soupcms/core/version.rb
@@ -0,0 +1,5 @@
+module Soupcms
+  module Core
+    VERSION = "0.0.1"
+  end
+end
