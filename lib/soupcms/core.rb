4560e958d233fb9a78451b9595d2184cadb96fc3
Sunit Parekh <parekh.sunit@gmail.com>
2014-04-12 23:26:19 +0530

    loading bootstrap css using bootstrap gem added support for customizing all bootstrap variables fixed hard coding of colors from ui modules

--- lib/soupcms/core.rb
+++ lib/soupcms/core.rb
@@ -1,3 +1,4 @@
+require 'bootstrap-sass'
 require 'soupcms/core/version'
 require 'soupcms/common'
 
--- ui/layout/bootstrap/left-sidebar/left-sidebar.js
+++ ui/layout/bootstrap/left-sidebar/left-sidebar.js
@@ -1,6 +1,1 @@
-//= require common/javascripts/bootstrap.js
-//= require common/javascripts/responsive-img.js
-//= require common/javascripts/holder.js
-//= require common/javascripts/raphael-min.js
-//= require common/javascripts/underscore-min.js
-//= require common/javascripts/sequence-diagram-min.js
+//= require common/javascripts/default.js
