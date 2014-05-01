90b3af8427cf9d83cda9e3021ab602f2ff0a34ec
Sunit Parekh <parekh.sunit@gmail.com>
2014-05-01 18:08:43 +0530

    added module to article css namespace

--- ui/module/bootstrap/article/article.js
+++ ui/module/bootstrap/article/article.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $(".article table").addClass('table table-striped table-bordered table-hover')
+    $(".module-article table").addClass('table table-striped table-bordered table-hover')
 
     if ($(".sequence-diagram").sequenceDiagram) {
         $(".sequence-diagram").sequenceDiagram({theme: 'hand'});
